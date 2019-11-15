f28320c20b23f8a4890a0b40e56de85213f6be29
pranita-sawant <1995sawantpranita@gmail.com>
2019-11-16 00:13:43 +0530

    Upload image

--- app/src/main/java/com/example/emenu/AddMenuActivity.kt
+++ app/src/main/java/com/example/emenu/AddMenuActivity.kt
@@ -1,5 +1,12 @@
 package com.example.emenu
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.*
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -8,9 +15,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_acc_set.*
+import kotlinx.android.synthetic.main.activity_add_menu.*
 
-class AddMenuActivity : AppCompatActivity() {
+
+class AddMenuActivity : AppCompatActivity()  {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_menu)
@@ -26,36 +35,105 @@ class AddMenuActivity : AppCompatActivity() {
         val menu_price = findViewById<EditText>(R.id.et_menuprice)
         val db = FirebaseFirestore.getInstance()
 
-        addMenu.setOnClickListener {
-            val TAG = "MyMessage"
-            // Create a new user with a first and last name
-            val menuItem = hashMapOf(
-                "Menu Name" to menu_name.text.toString(),
-                "Menu Desc" to menu_desc.text.toString(),
-                "Menu Price" to menu_price.text.toString()
-            )
+        //BUTTON CLICK
+        btn_choose_image.setOnClickListener {
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
 
-            db.collection("MenuItems")
-                .add(menuItem as Map<String, Any>)
-                .addOnSuccessListener { documentReference ->
-                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-                    Toast.makeText(
-                        this@AddMenuActivity,
-                        "New Menu Item added successfully.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+        addMenu.setOnClickListener {
+            val TAG = "Adding Menu"
+
+                val menuItem = hashMapOf<String, Any>(
+                    "Menu Name" to menu_name.text.toString(),
+                    "Menu Desc" to menu_desc.text.toString(),
+                    "Menu Price" to menu_price.text.toString()
+                )
+
+                db.collection("MenuItems")
+                    .add(menuItem as Map<String, Any>)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                        Toast.makeText(
+                            this@AddMenuActivity,
+                            "New Menu Item added successfully.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                        Toast.makeText(
+                            this@AddMenuActivity,
+                            "Menu Item addition failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                menu_name.text.clear()
+                menu_desc.text.clear()
+                menu_price.text.clear()
+                image_upload.setImageDrawable(null)
+            }
+        }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
                 }
-                .addOnFailureListener { e ->
-                    Log.w(TAG, "Error adding document", e)
-                    Toast.makeText(
-                        this@AddMenuActivity,
-                        "Menu Item addition failed",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
-            menu_name.text.clear()
-            menu_desc.text.clear()
-            menu_price.text.clear()
+            }
         }
     }
+
+    //handle result of picked image
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            image_upload.setImageURI(data?.data)
+        }
+    }
+
+
+
+
+
 }
+
+
