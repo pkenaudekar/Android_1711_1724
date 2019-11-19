09887ef0b8d75f0594f4c711bcde284696220c50
pranita <1995sawantpranita@gmail.com>
2019-11-19 12:19:27 +0530

    Image upload and customer menu

---
+++ app/src/main/java/com/example/emenu/CustMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.emenu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CustMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cust_menulist)
+    }
+}
--- app/src/main/java/com/example/emenu/MenuListAdapter.kt
+++ app/src/main/java/com/example/emenu/MenuListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.emenu
 
 import android.content.Context
 import android.content.Intent
+import android.media.Image
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,19 +13,18 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emenu.data.MenuItem
 import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_menulist.view.*
 
-class MenuListAdapter (private val menuList: MutableList<MenuItem>,
-                      private val context : Context,
-                      private val firestoreDB: FirebaseFirestore): RecyclerView.Adapter<MenuListAdapter.ViewHolder>() {
+class MenuListAdapter(
+    private var menuList: MutableList<MenuItem>,
+    private var context: Context,
+    private val firestoreDB: FirebaseFirestore
+) : RecyclerView.Adapter<MenuListAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val list = menuList[position]
-        holder.menuName.text = list.menuName
-        holder.price.text = list.menuPrice
-        Log.d("Stuff",list.menuName)
-
-        holder.edit.setOnClickListener{updateMenuList(list)}
-        holder.delete.setOnClickListener { deleteMenuList(list.id!!, position) }
+        holder.bind(list, position)
     }
 
     private fun updateMenuList(list: MenuItem) {
@@ -32,6 +32,7 @@ class MenuListAdapter (private val menuList: MutableList<MenuItem>,
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.putExtra("UpdateMenuId", list.id)
         intent.putExtra("UpdateMenuName", list.menuName)
+        intent.putExtra("UpdateMenuDesc", list.menuDesc)
         intent.putExtra("UpdateMenuPrice", list.menuPrice)
         context.startActivity(intent)
     }
@@ -54,18 +55,28 @@ class MenuListAdapter (private val menuList: MutableList<MenuItem>,
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
-            LayoutInflater.from(parent!!.context).inflate(R.layout.activity_menulist, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_menulist, parent, false)
 
         return ViewHolder(view)
     }
 
     inner class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
         internal var menuName: TextView = view.findViewById(R.id.tvTitle)
-        internal var price: TextView = view.findViewById(R.id.tvContent)
+        internal var menuDesc: TextView = view.findViewById(R.id.tvContent)
+        internal var price: TextView = view.findViewById(R.id.tvPrice)
         internal var edit: ImageView = view.findViewById(R.id.ivEdit)
         internal var delete: ImageView = view.findViewById(R.id.ivDelete)
+        internal var itemImage: ImageView = view.findViewById(R.id.imageView_menuImg)
 
+        fun bind(menuItem: MenuItem, position: Int) {
+            menuName.text = menuItem.menuName
+            menuDesc.text = menuItem.menuDesc
+            price.text = menuItem.menuPrice
+            Picasso.get().load(menuItem.imageUrl).into(itemImage)
+            Log.d("Stuff", menuItem.menuName!!)
+
+            edit.setOnClickListener { updateMenuList(menuItem) }
+            delete.setOnClickListener { deleteMenuList(menuItem.id!!, position) }
+        }
     }
-
-
 }
